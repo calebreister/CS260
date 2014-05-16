f8471bf4a9df9c4e505891ff84cca8abab791f08
Caleb Reister <calebreister@gmail.com>
2014-05-15 22:11:41 -0700

    Merge sort complete
    Combined the following examples:
    
    void mergeTwoSortedArrays(const int a1[], const int a2[],
    int mergedArray[], int n) {
    
        int i = 0;
        int j = 0;
        int k = 0;
    
        // Iterate and compare two input arrays until at least
    one of them reaches to boundary.
        while (i < n && j < n) {
            if (a1[i] < a2[j]) {
                mergedArray[k++] = a1[i++];
            }
            else if (a1[i] > a2[j]) {
                mergedArray[k++] = a2[j++];
            }
            else if (a1[i] == a2[j]) {
                mergedArray[k++] = a1[i++];
                mergedArray[k++] = a2[j++];
            }
        }
    
        // Copy the remaining items from the other array,
    without comparison until, boundary.
        while (i < n) mergedArray[k++] = a1[i++];
        while (j < n) mergedArray[k++] = a2[j++];
    
    }
    
    ////////////////////////////////////////////////////////////
    # split in half
    m = n / 2
    
    # recursive sorts
    sort a[1..m]
    sort a[m+1..n]
    
    # merge sorted sub-arrays using temp array
    b = copy of a[1..m]
    i = 1, j = m+1, k = 1
    while i <= m and j <= n,
        a[k++] = (a[j] < b[i]) ? a[j++] : b[i++]
        → invariant: a[1..k] in final position
    while i <= m,
        a[k++] = b[i++]
        → invariant: a[1..k] in final position
    
    ///////////////////////////////////////////////////////
    Several othero were from PDF files

--- Sort/src/Sort.cc
+++ Sort/src/Sort.cc
@@ -2,38 +2,97 @@
  * @author Caleb Reister <calebreister@gmail.com>
  */
 
-#include <array>
 #include <fstream>
 #include <iostream>
-#include <cstdlib>
 #include <chrono>
+#include <algorithm>
+#include <cstdlib>
+#include <cstdint>
+using namespace std;
 
 namespace sort {
-    template<std::size_t SIZE>
-    void radix(std::array<int, SIZE>& a);
-
-    template<std::size_t SIZE>
-    void merge(std::array<int, SIZE>& a);
-
-    template<std::size_t SIZE>
-    void quick3(std::array<int, SIZE>& a);
+    void radix(int data[], uint32_t size);
+    void merge(int data[], uint32_t first, uint32_t last);
+    void quick(int data[], uint32_t size);
 }
 
 int main() {
+    int x[8] = {3, 5, 1, 2, 4, 0, 10, 3};
+    for (int i = 0; i < 8; i++)
+        cout << x[i] << endl;
+    sort::merge(x, 0, 8);
+    cout << endl;
+    for (int i = 0; i < 8; i++)
+        cout << x[i] << endl;
+}
+
+void sort::radix(int data[], uint32_t size) {
 
 }
 
-template<std::size_t SIZE>
-void sort::radix(std::array<int, SIZE>& a) {
+void sort::merge(int data[], uint32_t first, uint32_t last) {
+    const uint32_t SIZE = last - first;
+    const uint32_t mid = (last - first) / 2;
 
+    if (SIZE == 1)
+        return;
+    else if (SIZE == 2)
+    {
+        if (data[0] > data[1])
+            swap(data[0], data[1]);
+    }
+    else
+    {
+        //separate
+        int* left = new int[mid];
+
+        const uint32_t SIZE_right = last - mid;
+        int* right = new int[SIZE_right];
+
+        for (uint32_t i = 0; i < mid; i++)
+            left[i] = data[i];
+
+        uint32_t r = 0;
+        for (uint32_t d = mid; d < last; d++)
+        {
+            //d = data index
+            //r = right index
+            right[r++] = data[d];
+        }
+
+        ///////////////////////////////////////
+        sort::merge(left, 0, mid);
+        sort::merge(right, 0, SIZE_right);
+
+        ///////////////////////////////////////
+        //merge
+        uint32_t l = 0; //left index
+        r = 0;          //right index
+        uint32_t d = 0; //data index
+        while (l < mid && r < SIZE_right)
+        {
+            if (left[l] < right[r])
+                data[d] = left[l++];
+            else if (right[r] < left[l])
+                data[d] = right[r++];
+            else
+            {
+                data[d++] = right[r++];
+                data[d] = left[l++];
+            }
+            d++;
+        }
+        //deal with uneven datasets
+        while (l < mid)
+            data[d++] = left[l++];
+        while (r < mid)
+            data[d++] = right[r++];
+
+        delete [] left;
+        delete [] right;
+    }
 }
 
-template<std::size_t SIZE>
-void sort::merge(std::array<int, SIZE>& a) {
-
-}
-
-template<std::size_t SIZE>
-void sort::quick3(std::array<int, SIZE>& a) {
+void sort::quick(int data[], uint32_t size) {
 
 }

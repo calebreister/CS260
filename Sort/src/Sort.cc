a953a6b8aa1bd5bc519108977ded460eb1f62b3a
Caleb Reister <calebreister@gmail.com>
2014-05-16 09:18:19 -0700

    Radix sort implemented

--- Sort/src/Sort.cc
+++ Sort/src/Sort.cc
@@ -6,12 +6,14 @@
 #include <iostream>
 #include <chrono>
 #include <algorithm>
+#include <forward_list>
 #include <cstdlib>
 #include <cstdint>
+#include <cmath>
 using namespace std;
 
 namespace sort {
-    void radix(int data[], uint32_t size);
+    bool radix(int data[], uint32_t size);
     void merge(int data[], uint32_t first, uint32_t last);
     void quick(int data[], uint32_t size);
 }
@@ -20,16 +22,77 @@ int main() {
     int x[8] = {3, 5, 1, 2, 4, 0, 10, 3};
     for (int i = 0; i < 8; i++)
         cout << x[i] << endl;
-    sort::merge(x, 0, 8);
+    sort::radix(x, 8);
     cout << endl;
     for (int i = 0; i < 8; i++)
         cout << x[i] << endl;
 }
 
-void sort::radix(int data[], uint32_t size) {
+/**@brief Implements a radix sort, only works for positive integers
+   @param data The array to sort
+ */
+bool sort::radix(int data[], uint32_t size) {
+    //make sure there are no negative values and find the largest value
+    int places = data[0]; //will store the number of digits
+    for (uint32_t i = 0; i < size; i++)
+    {
+        if (data[i] < 0)
+            return false;
+        if (data[i] > places)
+            places = data[i];
+    }
+    places = log10(places) + 1; //the number of decimal places
 
+    //create an array of linked lists to store the values of each place in
+    //[0] stores data that is in the 1's place
+    //[1] stores data that is in the 10's place...
+    forward_list<int>* bucket = new forward_list<int>[places];
+
+    //build lists in order
+    for (uint32_t d = 0; d < size; d++) //data iteration
+    {
+        uint32_t p = log10(data[d]); //place to use
+        //insert data into list
+        //data is lowest in place
+        if (bucket[p].empty() || data[d] < bucket[p].front())
+            bucket[p].push_front(data[d]);
+        else
+        {
+            //list iterators
+            forward_list<int>::iterator l_lead = bucket[p].begin();
+            forward_list<int>::iterator l_trail;
+            while (l_lead != bucket[p].end() && *l_lead < data[d])
+            {
+                l_trail = l_lead;
+                l_lead++;
+            }
+            bucket[p].insert_after(l_trail, data[d]);
+        } //else
+    } //data
+
+    //move sorted data from the bucket array to the data array
+    uint32_t d = 0; //data iteration
+    //d does not need to be checked, we know that the exact number
+    //of list elements matches the size of d
+    for (uint32_t p = 0; p < places; p++) //place iteration
+    {
+        while (!bucket[p].empty()) //empty list
+        {
+            data[d++] = bucket[p].front();
+            bucket[p].pop_front();
+        } //empty
+    } //place
+
+    delete [] bucket;
+    return true;
 }
 
+/**@brief Implements a complete recursive merge sort (low values on top),
+          entirely self-contained.
+   @param data The array to sort
+   @param first The starting position in the array
+   @param last The ending position in the array (1 + index)
+ */
 void sort::merge(int data[], uint32_t first, uint32_t last) {
     const uint32_t SIZE = last - first;
     const uint32_t mid = (last - first) / 2;
